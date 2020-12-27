3409f3acdecf7b629b74fdcc846577bd5c3e94f8
Jonas Ward <ward@ugd.ai>
2020-12-28 00:23:32 +0100

    (some more tests)

---
+++ tests/gyroids_loop.py
@@ -0,0 +1,37 @@
+from data.grid import TPMSGrid
+from vis.grid import vis_grid, vis_grid_idx, vis_grid_pseudocolors, video_output_grid_pseudocoloring
+from functions.tpms import *
+import math
+
+# x_dim, y_dim = 1280, 720
+x_dim, y_dim = 1000, 1000
+
+translation = .5, .5
+rotation = 0.
+
+grid = TPMSGrid(x_dim, y_dim)
+grid.transform_idx_grid(rotation, translation)
+# vis_grid(grid)
+# print(grid.grid)
+
+grid3 = TPMSGrid(x_dim, y_dim)
+grid3.transform_idx_grid(rotation, translation)
+gyroid3 = Gyroid(50.0, 12.0, 35.0)
+gyroid3.apply_grid(grid3, .5)
+
+grid2 = TPMSGrid(x_dim, y_dim)
+grid2.transform_idx_grid(rotation, translation)
+gyroid2 = Gyroid(grid3.grid, 10.0, 30.0)
+gyroid2.apply_grid(grid2, .2)
+
+gyroid = Gyroid(grid3.grid, 1.0, 1.0)
+gyroid.apply_grid(grid, 0)
+# vis_grid(grid)
+# vis_grid_pseudocolors(grid)
+z_vals = [(math.cos(z * math.pi / 100.0) + 1) * 500 for z in range(200)]
+
+# print(z_vals)
+video_output_grid_pseudocoloring(grid, gyroid, z_vals, "gyroid_3_loop_5.mp4")
+# print(grid.grid)
+
+# ToDo: json saving configuration of different tpmses, grid and pseudocolor map 
--- tests/tpms_tests.py
+++ tests/tpms_tests.py
@@ -4,7 +4,7 @@ from functions.tpms import *
 import math
 
 # x_dim, y_dim = 1280, 720
-x_dim, y_dim = 1080, 1920
+x_dim, y_dim = 1000, 1000
 
 translation = .5, .5
 rotation = 0.
@@ -14,24 +14,24 @@ grid.transform_idx_grid(rotation, translation)
 # vis_grid(grid)
 # print(grid.grid)
 
-# grid3 = TPMSGrid(x_dim, y_dim)
-# grid3.transform_idx_grid(rotation, translation)
-# gyroid3 = FischerKoch(50.0, 4.0, 50.0)
-# gyroid3.apply_grid(grid3, .5)
+grid3 = TPMSGrid(x_dim, y_dim)
+grid3.transform_idx_grid(rotation, translation)
+gyroid3 = FischerKoch(50.0, 4.0, 50.0)
+gyroid3.apply_grid(grid3, .5)
 
-grid2 = TPMSGrid(x_dim, y_dim)
-grid2.transform_idx_grid(rotation, translation)
-gyroid2 = Neovius(grid.grid, 10.0, 40.0)
-gyroid2.apply_grid(grid2, .2)
+# grid2 = TPMSGrid(x_dim, y_dim)
+# grid2.transform_idx_grid(rotation, translation)
+# gyroid2 = Neovius(grid3.grid, 10.0, 40.0)
+# gyroid2.apply_grid(grid2, .2)
 
-gyroid = Gyroid(grid2.grid, 1.0, 1.0)
+gyroid = Gyroid(grid3.grid, 1.0, 1.0)
 gyroid.apply_grid(grid, .5)
 # vis_grid(grid)
-vis_grid_pseudocolors(grid)
-z_vals = [math.sin(z / 435.0) * 12000 for z in range(50)]
+# vis_grid_pseudocolors(grid)
+z_vals = [(math.cos( (z / 200 + 1) * math.pi) + 1) * 2000 for z in range(400)]
 
 # print(z_vals)
-video_output_grid_pseudocoloring(grid, gyroid, z_vals, "neovius.mp4")
+video_output_grid_pseudocoloring(grid, gyroid, z_vals, "fisher_koch_rec.mp4")
 # print(grid.grid)
 
 # ToDo: json saving configuration of different tpmses, grid and pseudocolor map 
